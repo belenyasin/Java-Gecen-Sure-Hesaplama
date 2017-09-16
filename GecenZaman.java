49f8775b8c89d09fed3227a0ec47f1ff75f61f2c
Yasin Rıdvan Belen <belenyasin@gmail.com>
2017-09-16 23:11:51 +0300

    Add files via upload

---
+++ GecenZaman.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author belen.yasin
+ * belenyasin@gmail.com
+ */
+public class GecenZaman {
+
+  public static void main(String[] args) {
+    long beginTime = 0, endTime = 0;
+
+    beginTime = System.currentTimeMillis();
+
+    for (int i = 0; i < 10; i++) {
+      System.out.println("Durum: " + i);
+    }
+
+    endTime = System.currentTimeMillis();
+
+    System.out.println("Çalışma Süresi : " + ((double) (endTime - beginTime)) / 1000);
+  }
+}
